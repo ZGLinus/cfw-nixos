dcc4d4a33d59fede2e01d9986016e07ef44da97c
zglinus <zglinus@163.com>
2023-04-13 10:20:21 +0800

    pkgs update again

--- default.nix
+++ default.nix
@@ -18,8 +18,8 @@
   cfw = pkgs.callPackage ./pkgs/cfw/default.nix { }; 
   obsidian = pkgs.callPackage ./pkgs/obsidian/default.nix { };
   watt-toolkit =  pkgs.callPackage ./pkgs/watt-toolkit/default.nix { };
-  latte-dock = libsForQt5.callPackage ./pkgs/latte-dock/default.nix { };
-  wps = libsForQt5.callPackage ./pkgs/wps/default.nix { };
+  latte-dock = pkgs.libsForQt5.callPackage ./pkgs/latte-dock/default.nix { };
+  wps = pkgs.libsForQt5.callPackage ./pkgs/wps/default.nix { };
   sddm = pkgs.callPackage ./pkgs/sddm/default.nix { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...

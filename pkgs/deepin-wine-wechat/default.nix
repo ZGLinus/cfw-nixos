26dc18c735b08530923fc346621049c79d858509
zglinus <zglinus@163.com>
2023-04-27 12:39:22 +0800

    :boom: add wechat/tim with deepin-wine5

--- pkgs/deepin-wine-wechat/default.nix
+++ pkgs/deepin-wine-wechat/default.nix
@@ -7,12 +7,14 @@
 , pkgs
 , bash
 , p7zip
+, coreutils
+, zstd
 , ...
 } @ args:
 
 pkgsi686Linux.stdenv.mkDerivation rec {
   pname = "deepin-wine-wechat";
-  version = "3.5.0.4";
+  version = "3.5.0.46";
   src = fetchurl {
     url = "https://com-store-packages.uniontech.com/appstore/pool/appstore/c/com.qq.weixin.deepin/com.qq.weixin.deepin_3.4.0.38deepin6_i386.deb";
     sha256 = "sha256-bZjto1n7+MS4k7rhXN2mBZDtfT1ABvmXeuQLxje2Srk=";
@@ -21,11 +23,6 @@ pkgsi686Linux.stdenv.mkDerivation rec {
   font = fetchurl {
     url = "https://media.githubusercontent.com/media/zglinus-for-nix/nix-source/master/Applications/wine/fake_simsun.tar.gz";
     sha256 = "sha256-lWAhpcLVl8D6ui0Zn2CmG7L3NP6k+TBwItquzSQ9DS0=";
-  };
-
-  wechat = fetchurl {
-    url = "https://media.githubusercontent.com/media/zglinus-for-nix/nix-source/master/Applications/wine/WeChat3.5.0.4.tar.gz";
-    sha256 = "sha256-Zx34H4rCc1IyW9AnNCm9Eioixykuf7xYk5F0gfw0X2M=";
   };
 
   mmmojo = fetchurl {
@@ -38,8 +35,13 @@ pkgsi686Linux.stdenv.mkDerivation rec {
     sha256 = "sha256-s13m2klObKel2h7yknWSJO2hLMfqsK6aWC8pUxTheAA=";
   };
 
+  deepin-wine-wechat-arch = fetchurl {
+    url = "https://github.com/vufa/deepin-wine-wechat-arch/releases/download/v3.5.0.46-1/deepin-wine-wechat-3.5.0.46-1-x86_64.pkg.tar.zst";
+    sha256 = "sha256-CAvcplFSOBFMQfWKkGnJjMgBNta5eSAgxmmRrNoJIeA=";
+  };
+
   # autoPatchelfHook 可以自动修改二进制文件
-  nativeBuildInputs = [ autoPatchelfHook makeWrapper p7zip ];
+  nativeBuildInputs = [ autoPatchelfHook makeWrapper p7zip coreutils zstd ];
 
   buildInputs = with pkgsi686Linux;[
     xorg.libXext
@@ -51,9 +53,10 @@ pkgsi686Linux.stdenv.mkDerivation rec {
     ar x ${src}
     tar xf data.tar.xz
     tar xf ${font}
-    tar xf ${wechat}
     tar xf ${mmmojo}
     tar xf ${runfile}
+    mkdir src
+    tar -xf ${deepin-wine-wechat-arch} -C src
   '';
 
   installPhase = ''
@@ -61,17 +64,12 @@ pkgsi686Linux.stdenv.mkDerivation rec {
     mkdir -p $out/files
     mkdir -p $out/lib
     mkdir -p $out/share
-    7z x -aoa "opt/apps/*/files/files.7z" -o"temp"
-    rm -r "temp/drive_c/Program Files/Tencent/WeChat"
-    install -m644 "WeChat3.5.0.4.exe" "temp/drive_c/Program Files/Tencent/"
-    install -m644 "mmmojo.dll" "temp/drive_c/Program Files/Tencent/"
-    install -m644 "mmmojo_64.dll" "temp/drive_c/Program Files/Tencent/"
-    # Creating 'XPlugin/Plugins/XWeb' to forbid wechat browser creating crash logs
-    mkdir -p "temp/drive_c/users/@current_user@/Application Data/Tencent/WeChat/XPlugin/Plugins/"
-    touch "temp/drive_c/users/@current_user@/Application Data/Tencent/WeChat/XPlugin/Plugins/XWeb"
+    7z x -aoa "src/opt/apps/*/files/files.7z" -o"temp"
+    rm temp/drive_c/windows/Fonts/wqy*
     mv fake_simsun.ttc $out/files
     ln -sf "$out/files/fake_simsun.ttc" "temp/drive_c/windows/Fonts/fake_simsun.ttc"
     7z a -t7z -r "$out/files/files.7z" "./temp/*"
+    md5sum "$out/files/files.7z" | awk '{ print $1 }' > "$out/files/files.md5sum"
     mv run.sh $out/files
     mv opt/apps/*/files/dlls/* $out/lib/
     mv opt/apps/*/entries/* $out/share/
@@ -80,16 +78,16 @@ pkgsi686Linux.stdenv.mkDerivation rec {
       s|START_SHELL_PATH=.*|START_SHELL_PATH=\$(whereis run_v4.sh \| cut -b 12-80)|
       s|export DEB_PACKAGE_NAME="com.qq.weixin.deepin"||
       s|deepin-wine6-stable|deepin-wine5|
-      s|ARCHIVE_FILE_DIR=.*|ARCHIVE_FILE_DIR=$out/opt/file.7z|
-      s|export WINEPREDLL=.*|export WINEPREDLL=$out/lib|
-      s|WINEPREDLL=\"\$ARCHIVE_FILE_DIR/dlls\"|WINEPREDLL=\$WINEPREDLL|
-      s|export LD_LIBRARY_PATH=.*|export LD_LIBRARY_PATH=/nix/store/lwvv4fns4d05h22cjd9r8axblgmdcjc3-freetype-2.12.1/lib|
+      s|ARCHIVE_FILE_DIR=.*|ARCHIVE_FILE_DIR=$out/files/files.7z|
+      s|WINEPREDLL=\"\$ARCHIVE_FILE_DIR/dlls\"|WINEPREDLL=\"$out/lib\"|
+      s|WINEDLLPATH=/opt/\$APPRUN_CMD/lib:/opt/\$APPRUN_CMD/lib64|WINEDLLPATH=/nix/store/s0p49c2hdxfa32vh65w58dzn8s28wvqd-deepin-wine-5.0.16/lib|
       24 i export OUTPATH=$out
-      s|WECHAT_INSTALLER-|WECHAT_INSTALLER|
-      s|WeChatSetup|WeChat|
+      s|/opt/apps/\$DEB_PACKAGE_NAME/files/files.md5sum|$out/files/files.md5sum|
+      s|LD_LIBRARY_PATH=/opt/apps/\$DEB_PACKAGE_NAME/files/lib32|LD_LIBRARY_PATH=$out/lib|
+      s|/opt/deepinwine/tools/sendkeys.exe|\$(whereis sendkeys.exe \| awk '{ print \$2 }')|
     " "$out/files/run.sh"
     sed -i "
-      s|/opt/apps/com.qq.weixin.deepin/files|$out/opt/run.sh|
+      s|/opt/apps/com.qq.weixin.deepin/files|$out/files|
       s|wine6|wine5|
     " "$out/share/applications/com.qq.weixin.deepin.desktop"
   '';
